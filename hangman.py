db5deb54d8d5e524ccf6634c26b57a132512ca15
evakdad <ivakdad@gmail.com>
2021-07-11 14:18:33 +0430

    re-writing the game with classes\

--- hangman.py
+++ hangman.py
@@ -25,19 +25,123 @@ for i in sheet1.range('a1:a400', returnas='matrix'):
 words=['abandon', 'abandonment', 'abbreviation', 'abeyance', 'abide', 'ability', 'abnormal', 'aboard', 'abolish', 'abolition', 'abortion', 'abortive', 'abridge', 'abrogate', 'abrupt', 'absence', 'absent', 'absolute', 'absolutely', 'absorb', 'absorption', 'abstract', 'absurd', 'absurdity', 'abundance', 'abundant', 'abuse', 'academic', 'academy', 'accede', 'accelerate', 'acceleration', 'access', 'accessible', 'accessory', 'accident', 'accidental', 'accidentally', 'accommodate', 'accommodation', 'accompaniment', 'accompany', 'accomplish', 'accomplishment', 'accord', 'account', 'accountant', 'accounting', 'accrue', 'accumulate', 'accumulation', 'accuracy', 'accurate', 'accusation', 'accuse', 'achieve', 'achievement', 'acid', 'acknowledge', 'acquaint', 'acquaintance', 'acquainted', 'acquire', 'acquisition', 'across', 'activate', 'actively', 'actual', 'actually', 'acute', 'adapt', 'adaptation', 'additional', 'additive', 'address', 'adequate', 'adhere', 'adhesive', 'adjacent', 'adjoin', 'adjust', 'adjustment', 'administer', 'administration', 'administrative', 'admiration', 'admire', 'admission', 'admit', 'admittedly', 'adolescence', 'adolescent', 'adopt', 'adoption', 'adore', 'adorn', 'adornment', 'adult', 'advance', 'advanced', 'adventure', 'adventurous', 'adversary', 'adverse', 'adversity', 'advertise',
  'advisable', 'advocate', 'aerial', 'affect', 'affection', 'affectionate', 'affiliate', 'affirm', 'affirmation', 'affirmative', 'afford', 'affordable', 'agency', 'agent', 'aggravate', 'aggregate', 'aggregation', 'aggressive', 'aggressor', 'agitate', 'agitation', 'agony', 'agreeable', 'agreement', 'agriculture', 'aid', 'ailment', 'aim', 'air', 'aircraft', 'aisle', 'ajar', 'alarm', 'album', 'alert', 'alien', 'alienate', 'alignment', 'alike', 'allergic', 'allergy', 'alliance', 'allocate', 'allot', 'allowance', 'alloy', 'ally', 'alone', 'along', 'alongside', 'alter', 'alternate', 'alternation', 'alternative', 'altitude', 'aluminum', 'amateur', 'amaze', 'amazement', 'ambassador', 'ambiguity', 'ambiguous', 'ambition', 'ambitious', 'ambulance', 'amend', 'amendment', 'amends', 'amiable', 'amicable', 'amid', 'ammunition', 'amount', 'ample', 'amplification', 'amplify', 'amuse', 'analogy', 'analysis', 'analytical', 'analyze', 'ancestor', 'anchor', 'anecdote', 'angular', 'animate', 'animation', 'ankle', 'annex', 'anniversary', 'announce', 'announcement', 'announcer', 'annoy', 'annual', 'annually', 'antagonism', 'antagonist', 'antarctic', 'antibiotic', 'anticipate', 'anticipation', 'antique', 'antonym', 'anxiety', 'anxious', 'anyhow', 'apart', 'ape', 'apologize', 'apology', 'apparatus', 'apparent', 'appeal', 'appealing', 'appendix', 'appetite', 'applaud', 'appliance', 'applicable', 'applicant', 'application', 'apply', 'appoint', 'appointment', 'appreciable', 'appreciate', 'appreciation', 'appreciative', 'apprentice', 'approach', 'appropriate', 'approval', 'approve', 'approximate', 'approximately', 'apt', 'aptitude', 'arbitrary', 'arbitrator', 'arc', 'arch', 'archaeology', 'architect', 'architecture', 'ardent', 'arduous', 'area', 'arena', 'argue', 'arise', 'aristocracy', 'aristocrat', 'arithmetic', 'armour', 'arms', 'arouse', 'arrange', 'arrangement', 'array', 'arrest', 'arrogance', 'arrogant', 'article', 'artificial', 'ascend', 'ascertain', 'ascribe', 'ashamed', 'aside', 'aspect', 'aspirin', 'assassination', 'assault', 'assemble', 'assembly', 'assert', 'assess', 'assessment', 'assign', 'assignment', 'assist', 'assistance', 'assistant', 'associate', 'association', 'assorted', 'assortment', 'assume', 'assumption', 'assurance', 'assure', 'assured', 'astonish', 'astound', 'astray', 'astronomer', 'astronomical', 'astronomy', 'athlete', 'atlas', 'atmosphere', 'atom', 'atomic', 'attach', 'attachment', 'attack', 'attain', 'attempt', 'attend', 'attendance', 'attendant', 'attention', 'attentive', 'attic', 'attitude', 'attorney', 'attract', 'attraction', 'attribute', 'auction', 'audience', 'auditorium', 'august', 'author', 'authority', 'authorization', 'authorize', 'autobiography', 'automate', 'automatic', 'automation', 'automobile', 'autonomous', 'autonomy', 'auxiliary', 'avail', 'available', 'avenge', 'avenue', 'average', 'aviation', 'avoid', 'aware', 'awe', 'awful', 'awkward', 'awkwardly', 'axis', 'axle']
 
+
+class Errors: #or function?
+    pass
+
+class HangedMan:
+    """Gets level of hanging (number of wrong guesses) and returns related art."""
+
+    def __init__(self,level) -> None:
+        return self.levels[level]
+
+    face = "         --------        \n   	| /     |       \n   	|       |       \n  	|     ෴෴෴    \n   	|    (͡❛ ͜ʖ ❛)    \n   	|      ┏皿┛     \n   	|      / \      \n	|               \n                    "
+    body = "         --------        \n   	| /     |       \n   	|       |       \n  	|     ෴෴෴    \n   	|     (   )    \n   	|      ┏皿┛     \n   	|      / \      \n	|               \n                    "
+    head = "         --------        \n   	| /     |       \n   	|       |       \n  	|     ෴෴෴    \n   	|     (   )    \n   	|               \n   	|               \n	|               \n                    "
+    choobe = "         --------        \n   	| /     |       \n   	|       |       \n  	|               \n   	|              \n   	|               \n   	|               \n	|               \n                    "
+    dead = "         --------        \n   	| /     |       \n   	|       |       \n  	|     ෴෴෴    \n   	|     (*﹏*)    \n   	|      ┏皿┛     \n   	|      / \      \n	|               \n                    "
+    levels = {
+        1:choobe,
+        2: head,
+        3: body,
+        4: face,
+        5: dead
+    }
+
+class Message:
+
+    def say(message):
+        return random.choices(message)
+
+    intro = ["""Hi! Ready to play some Hangman?
+    If yes, type '1' to begin. 
+    If no, type '0' to end the program. \n"""]
+
+    first_guess = ["\n Here we go! Guess a letter: \n"]
+
+    next_guess= ["Guess another:"]
+
+    correct=[
+        'Correct!',
+        'Awesome!',
+        'Yess!',
+        'Woohoo!',
+        'You got it!'
+    ]
+
+    wrong=[
+        'oops!',
+        'nope :(',
+        "don't lose your head!"
+        ]
+
+    congrats=["You did it! Congrats!"]
+
+    fail = ["""\n oops :( 
+        the word was {}. You'll get it next time!"""]
+
+    replay = ["type 1 to play again, or type 0 to exit: \n"]
+
+    wrong_list = ["Wrong Guesses: {}   {} left"]
+
+
+
+
+class Game:
+    #The main game flow
+    def __init__(self, words, wrong_limit=5) -> None:
+        chosen_word:list = list(random.choice(words).lower())
+        length:int=len(chosen_word)
+        corrects:list = ['_' for i in range (length)]
+        wrongs:list=[]
+        wrong_limit=wrong_limit
+
+
+
+    def print_corrects(self) -> None:
+        print (*self.corrects)
+
+    def print_wrongs(self) -> None:
+        wrong_count = len(self.wrongs)
+        msg = Message.say(Message.wrong_list)
+
+        if wrong_count==0:
+            msg = msg.format('None',self.wrong_limit-wrong_count)
+        else:
+            msg = msg.format(self.wrongs.join(), self.wrong_limit - wrong_count)
+            
+        print (msg)
+
+    def print_hanged_man(self) -> str:
+
+        wrong_count=len(self.wrongs)
+        print (HangedMan(level=wrong_count))
+
+    def run(self):
+        play=True
+        while play is True:
+            pass
+
+
+
+
+
 #Error function
-def error_finder(g):
+def error_finder(entry):
+    errors = {'Duplicate': "You already entered that. Guess a new letter.",
+                'Nonletter': "That's not a letter. Please enter only letters.",
+                'OnlyOne': "Please enter only one letter."
+    }
     global wrongletters
     global userresult
-    flag=True
-    if g in wrongletters or g in userresult:
-        print("You already entered that. Guess a new letter.")
+    if entry in wrongletters or entry in userresult:
+        print(errors['Duplicate'])
         flag=False
-    if not g.isalpha():
-        print("That's not a letter. Please enter only letters.")
+    if not entry.isalpha():
+        print(errors['Nonletter'])
         flag=False
-    if len(g)>1:
-        print("Please enter only one letter.")
+    if len(entry)>1:
+        print(errors['OnlyOne'])
         flag=False
     return flag
 
@@ -52,15 +156,18 @@ dead = "         --------        \n   	| /     |       \n   	|       |       \n 
 hanged = [choobe, head, body, facethings, dead]
 
 #################
-begin = int(input("Hi! Ready to play some Hangman? If yes, type '1' to begin. If no, type '0' to end the program. \n"))
+msg = """Hi! Ready to play some Hangman?
+ If yes, type '1' to begin. 
+ If no, type '0' to end the program. \n"""
+begin = int(input(msg))
 while begin:
     begin=0
     myword=list(random.choice(words).lower())
-    length=len(myword) 
+    length=len(myword)
     userresult=[]
     for i in range (length):
         userresult.append("_")
-    
+
     print("\n Here we go! Guess a letter: \n")
     print(*userresult)
     guess=input().lower()
@@ -69,7 +176,7 @@ while begin:
 
     while userresult!=myword and len(wrongletters)<5:
         ##Errors
-        while error_finder(guess) is False: 
+        while error_finder(guess) is False:
             guess = input("Enter:\n").lower()
 
         if guess in myword:
@@ -78,10 +185,10 @@ while begin:
                 print ("Wrong Guesses:", *wrongletters, f" ({5-len(wrongletters)} left)")
             else:
                 print("Wrong Guesses: None", f" ({5-len(wrongletters)} left)")
-            for i in range (length): 
-                if myword[i]==guess: 
-                    userresult[i]=guess  
-        else: 
+            for i in range (length):
+                if myword[i]==guess:
+                    userresult[i]=guess
+        else:
             wrongletters.append(guess)
             print ("Wrong Guesses:",*wrongletters, f" ({5-len(wrongletters)} left)")
             print (hanged[len(wrongletters)-1])
@@ -95,5 +202,3 @@ while begin:
     elif len(wrongletters)>=5:
         print("\n oops :( the word was", "'"+"".join(myword)+"'", ". You'll get it next time!")
     begin = int(input("type 1 to play again, or type 0 to exit: \n"))
-
-
